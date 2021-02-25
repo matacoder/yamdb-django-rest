fd749eddb2e96f75bc2ef5e2f234aca8c3bbcda7
Denis Matakov <denis@matakov.com>
2021-02-25 10:12:59 +0300

    nginx based on image

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -137,8 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-staticMEDIA_URL = '/media/'
-
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'accounts.User'
