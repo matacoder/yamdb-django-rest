7d094883b134aae4b656296a79279f354a1f36f6
Denis Matakov <67960818+matacoder@users.noreply.github.com>
2021-09-17 10:17:12 +0300

    Update settings.py

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from datetime import timedelta
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # load_dotenv()
 
